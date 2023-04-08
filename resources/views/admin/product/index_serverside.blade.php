084e961e45515f780df7539f8307bc14204c0efb
isal48 <isalredarmy1@gmail.com>
2023-04-08 12:20:24 +0700

    update product

--- resources/views/admin/product/index_serverside.blade.php
+++ resources/views/admin/product/index_serverside.blade.php
@@ -31,9 +31,9 @@
 
 <div class="tables" style="margin-top:20px;">
    <div class="" style="margin-left:25px">
-      <a href="#" id="add_product" class="btn">Tambah Product</a>
+      {{-- <a href="#" id="add_product" class="btn">Tambah Product</a> --}}
 
-      <button class="btn btn-blue" data-toggle="modal2" data-target="#modal2">Click</button>
+      <button class="btn btn-blue" data-toggle="modal2" data-target="#modal2">Tambah product</button>
    </div>
    <section class="table__body">
       <table class="product-yajra">
@@ -63,25 +63,41 @@
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script>   
 
+Date.prototype.toDateInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+});
+
 var modal = document.getElementById("modal2");
 let navbar_atas = document.getElementsByClassName('top-bar');
 let navbar_atas_id = document.getElementById('top-bar');
 const navbar_samping_id = document.getElementById('sidebar');
 
+
+//input
+const name_ = document.getElementById('name_product')
+const harga_beli = document.getElementById('harga_beli')
+const berat = document.getElementById('total_satuan')
+const is_kg_check = document.getElementById('is_kg')
+const expired = document.getElementById('expired')
+//input
 const all_toogle = document.querySelectorAll('[data-toggle="modal2"]')
 all_toogle.forEach(btn => {
    btn.addEventListener('click', function(e) {
-      const name_ = document.getElementById('name_product')
+      
       const id_kategori = document.getElementById('kategori_select')
-      const harga_beli = document.getElementById('harga_beli')
       const cek_satuan = document.getElementById('is_kg')
-      const berat = document.getElementById('total_satuan')
-      const expired = document.getElementById('expired')
 
+      var date = new Date();
+      let dateplust1day = date.setDate(date.getDate() + 2);
+      var numDate= new Date(dateplust1day);
+    
       name_.value=''
       harga_beli.value=''
       berat.value=''
-
+      expired.value = numDate.toDateInputValue();
+     
       //ajax list kategori
       $.ajax({
          url: `{{route('kategori-list')}}`,
@@ -130,6 +146,12 @@ all_toogle.forEach(btn => {
 //    }
 // })
 
+function appendDelayedTextNode(element, text, delayMs) {
+    setTimeout(function () {
+        element.appendChild(document.createTextNode(text));
+    }, delayMs);
+}
+
 $('#form-product').on("submit", function(e) {
    e.preventDefault(); 
    let name = $("#name_product").val();
@@ -143,22 +165,30 @@ $('#form-product').on("submit", function(e) {
       $("#kategori_select").css('border-color','red')
    }
   
+   const cek_id_product = document.getElementById('id_product_hidden');
+
+  
    let data_pcs = {
-                     'nama_product' : name,
-                     'kategori_id' : id_kategori,
-                     'harga_beli' : harga_beli,
-                     'is_kg' : cek_satuan,
-                     'pcs':berat,
-                     'expired':expired
-                  };
-   let data_kg = {
                   'nama_product' : name,
                   'kategori_id' : id_kategori,
                   'harga_beli' : harga_beli,
                   'is_kg' : cek_satuan,
-                  'total_kg':berat,
+                  'pcs':berat,
                   'expired':expired
-                  }
+               };
+   let data_kg = {
+               'nama_product' : name,
+               'kategori_id' : id_kategori,
+               'harga_beli' : harga_beli,
+               'is_kg' : cek_satuan,
+               'total_kg':berat,
+               'expired':expired
+               }
+   if (cek_id_product) {
+      data_pcs.id_product = cek_id_product.value
+      data_kg.id_product = cek_id_product.value
+   }
+
    $.ajax({
          type:'post',
          headers: {
@@ -168,6 +198,7 @@ $('#form-product').on("submit", function(e) {
          data: cek_satuan == '1' ? data_kg : data_pcs,
          success:function(data) { 
             if (data.status == 'ok') {
+
                $('.product-yajra').DataTable().ajax.reload(null, true);
                   Swal.fire(
                            'Add',
@@ -182,8 +213,14 @@ $('#form-product').on("submit", function(e) {
                         }) 
             }
          },
-         error:function(err){
-            console.log(err);
+         error:function(xhr, status, error){
+            if (xhr.responseJSON.data.expired) {
+               console.log(xhr.responseJSON.data.expired);
+               let label_expired = document.getElementById('id_label_expired');
+               appendDelayedTextNode(label_expired, 'Tanggal tidak boleh kurang dari hari ini', 1000);
+            }
+           
+           
          }
    });
 });
@@ -214,12 +251,82 @@ $('#form-product').on("submit", function(e) {
    });
 </script>
 <script>
+   function selectElement(id, valueToSelect) {    
+      let element = document.getElementById(id);
+      element.value = valueToSelect;
+   }
+ //list kategori
+ $.ajax({
+   url: `{{route('kategori-list')}}`,
+   headers: {
+            'X-CSRF-TOKEN': '{{ csrf_token() }}'
+         },
+   type: 'get',
+   success: function(response_list_kategori){
+      setTimeout(() => {
+         if (response_list_kategori.data.data.length > 0) {
+            let html_opt = `<option value="0">- pilih kategori product-</option>`;
+            response_list_kategori.data.data.forEach((e) => {
+               html_opt += `<option value="${e.id_kategori}">${e.nama_kategori}</option>`;
+               if($("#kategori_select")) {
+                  $("#kategori_select").html(html_opt);
+               }
+            });
+         }
+      }, 1000);
+      
+   },
+   error: function(err){
+      console.log('error',err);
+   }
+})
+//list kategori
    function editProduct(id){
-      console.log(id);
+      $.ajax({
+         type: "post",
+         url:"{{ route('porudct-detailById') }}",
+         data:{'id_product':id},
+         success: function(response){
+            console.log(response.data);
+            //get
+            name_.value= response.data.nama_product;
+            harga_beli.value=response.data.harga_beli;
+            const cek_is_berat = response.data.is_kg == 0 ? response.data.pcs : response.data.total_kg;
+            berat.value= cek_is_berat
+            response.data.is_kg == 0 ? is_kg_check.checked = false : is_kg_check.checked =true;
+            selectElement('kategori_select',response.data.kategori_id)
+            let dd = new Date(Date.parse(response.data.expired));
+            expired.value = dd.toDateInputValue();
+            //get
+            const h3_title = document.getElementById('title_modal').textContent = 'Update Product';
+            var input = document.createElement("input");
+            input.type = "hidden";
+            input.setAttribute('id', 'id_product_hidden');
+            input.setAttribute('value', id);
+            document.getElementById('form-product').appendChild(input);  
+            const modal = document.querySelector('#modal2')
+            const modal_close = modal.querySelector('.modal__close_2')
+            modal.classList.add('show-modal')
+            navbar_atas_id.style.position = "initial";
+            navbar_samping_id.style.position = "initial";
+            modal_close.addEventListener('click', function(e){
+                  e.preventDefault()
+                  input.remove()
+                  modal.classList.remove('show-modal')
+                  navbar_atas_id.style.position = "fixed";
+                  navbar_samping_id.style.position = "fixed";
+            })
+         },
+         error: function(err){
+            console.log(err);
+         }
+      })
    }
 
    function editProductPrice(id){
-    
+      //ajax get byid
+      
+      //ajax getbyid
    }
 
    function deleteKategori(id){
--- resources/views/admin/product/modal_prodct.blade.php
+++ resources/views/admin/product/modal_prodct.blade.php
@@ -4,24 +4,24 @@
 
 <div class="modal2" id="modal2">
     <div class="modal__wrapper">
-        <h3 id="title_modal"></h3>
+        <h3 id="title_modal">Input Product</h3>
         <p></p>
         <form action="" id="form-product">
-            <label class="text-label-modal" for="nama_barang" style="margin-left: 5px;">Nama barang</label>
+            <label id="id_label_nama_barang" class="text-label-modal" for="nama_barang" style="margin-left: 5px;">Nama barang</label>
             <input class="input-modal-global" id="name_product" required type="text" style="margin-top:5px;" name="nama_barang" placeholder="Nama barang">
-            <label class="text-label-modal" for="kategori" style="margin-left: 5px;">Kategori</label>
+            <label id="id_label_kategori" class="text-label-modal" for="kategori" style="margin-left: 5px;">Kategori</label>
             <select class="input-modal-global" id="kategori_select" name="kategori"  style="margin-top:5px;" required>
               
             </select>
-            <label class="text-label-modal" for="harga_beli" style="margin-left: 5px;">Haraga Beli</label>
-            <input class="input-modal-global" id="harga_beli" required type="number" id="harga_beli" style="margin-top:5px;" name="harga_beli" placeholder="Harga beli">
-            <label class="text-label-modal" style="margin-left: 5px">Satuan kg, Uncheck bila pcs</label>
+            <label id="id_label_hargabeli" class="text-label-modal" for="harga_beli" style="margin-left: 5px;">Haraga Beli</label>
+            <input class="input-modal-global" id="harga_beli" min="1" required type="number" id="harga_beli" style="margin-top:5px;" name="harga_beli" placeholder="Harga beli">
+            <label id="id_label_satuan" class="text-label-modal" style="margin-left: 5px">Satuan kg, Uncheck bila pcs</label>
             <input  class="input-modal-global is_kg"  type="checkbox" id="is_kg" name="is_kg">
             <br>
-            <label class="text-label-modal" for="jumlah_berat" style="margin-left: 5px;">Jumlah berat / kg</label>
+            <label id="id_label_jumlah" class="text-label-modal" min="1" for="jumlah_berat" style="margin-left: 5px;">Jumlah berat / kg</label>
             <input class="input-modal-global" required type="number" id="total_satuan" style="margin-top:5px;" name="jumlah_berat" placeholder="Jumlah berat">
-            <label class="text-label-modal" for="expired" style="margin-left: 5px;">tanggal expired</label>
-            <input class="input-modal-global" required type="date" id="expired" style="margin-top:5px;" name="tanggal expired" placeholder="Tanggal expired">
+            <label id="" class="text-label-modal" for="expired" style="margin-left: 5px;">tanggal expired <label id="id_label_expired" style="color:red"></label> </label>
+            <input class="input-modal-global" required type="date" id="expired" style="margin-top:5px;" name="expired" placeholder="Tanggal expired">
             <div class="btn-group-modal2">
                 <button type="submit" class="btn-modal2 btn-blue">Submit</button>
                 <button class="btn-modal2 btn-light-modal2 modal__close_2">Cancel</button>
