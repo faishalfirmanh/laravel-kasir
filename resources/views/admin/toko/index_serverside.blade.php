476c530a97ff51365b0d17cf19a7d13b5d1e7c18
isal48 <isalredarmy1@gmail.com>
2023-04-10 12:21:59 +0700

    tampilan crud toko

--- resources/views/admin/toko/index_serverside.blade.php
+++ resources/views/admin/toko/index_serverside.blade.php
@@ -37,6 +37,10 @@
 </div>
 @endsection
 
+@section('modal_global')
+    @include('admin.toko.modal')
+@endsection
+
 @push('scripts')
 <script type="text/javascript">
     $(function () { 
@@ -60,12 +64,152 @@
      });
 </script>
 <script>
-   function editToko(id){
+   const input_toko = document.getElementById("nama_toko")
+   const all_toogle = document.querySelectorAll('[data-toggle="modaltoko"]')
+   let navbar_atas_id = document.getElementById('top-bar');
+   const navbar_samping_id = document.getElementById('sidebar');
+   const modal = document.querySelector("#modaltoko")
 
-   }
+   $("#add_toko").on("click",function(e){
+     
+      const modal_close = modal.querySelector('.modal__close_2')
+      modal.classList.add('show-modal')
+      navbar_atas_id.style.position = "initial";
+      navbar_samping_id.style.position = "initial";
+      input_toko.value = ""
+      modal_close.addEventListener('click', function(e){
+         e.preventDefault()
+         modal.classList.remove('show-modal')
+         navbar_atas_id.style.position = "fixed";
+         navbar_samping_id.style.position = "fixed";
+      })
 
-   function deleteToko(id){
+      //ajax 
+      if(document.getElementById("id_toko_hidden")){
+         document.getElementById("id_toko_hidden").removeAttribute("id")
+      }
+   })
+
+</script>
+<script>
+
+   $("#form-toko").on("submit",function(e){
+      e.preventDefault()
+      const cek_id =  document.getElementById('id_toko_hidden');
       
+      let data_input = {
+         'nama_toko' : input_toko.value
+      }
+      if (cek_id) {
+         data_input.id_toko = localStorage.getItem('id_toko_hidden')
+      }
+
+      console.log();
+      $.ajax({
+         type:'post',
+         data:data_input,
+         headers: {
+            'X-CSRF-TOKEN': '{{ csrf_token() }}'
+         },
+         url:`{{route('post-toko')}}`,
+         success:function(res){
+            if (res.status == 'ok') {
+               $('.toko-yajra').DataTable().ajax.reload(null, true);
+                  Swal.fire(
+                           'Add',
+                           'Save data toko berhasil',
+                           'success'
+                        ).then((result) => {
+                           if (result.isConfirmed) {
+                              modal.classList.remove('show-modal')
+                              navbar_atas_id.style.position = "fixed";
+                              navbar_samping_id.style.position = "fixed";
+                           } 
+                        }) 
+               }
+         }
+      })
+      
+   })
+
+   function editToko(id){
+      const modal = document.querySelector("#modaltoko")
+      const modal_close = modal.querySelector('.modal__close_2')
+      modal.classList.add('show-modal')
+      navbar_atas_id.style.position = "initial";
+      navbar_samping_id.style.position = "initial";
+
+      input_toko.value = ""
+
+      //ajax
+      $.ajax({
+         type:'post',
+         data:{'id_toko':id},
+         headers: {
+            'X-CSRF-TOKEN': '{{ csrf_token() }}'
+         },
+         url:`{{route('detail-toko')}}`,
+         success:function(res){
+            let data = res.data;
+            input_toko.value = data.nama_toko
+            //create input tipe hidden for edit
+            var input = document.createElement("input");
+            input.type = "hidden";
+            input.setAttribute('id', 'id_toko_hidden');
+            input.setAttribute('value', parseInt(id));
+            document.getElementById('form-toko').appendChild(input); 
+            localStorage.setItem("id_toko_hidden", id);
+            const idnya  = localStorage.getItem("id_toko_hidden");
+            console.log(idnya);
+            //create input tipe hidden for edit
+            
+         },
+         error:function(xhr,status, msg){
+
+         }
+      })
+      //ajax
+
+      modal_close.addEventListener('click', function(e){
+         e.preventDefault()
+         modal.classList.remove('show-modal')
+         navbar_atas_id.style.position = "fixed";
+         navbar_samping_id.style.position = "fixed";
+      })
+      console.log(id);
    }
+
+function deleteToko(id){
+   Swal.fire({
+         title: "Yakin ingin menghapus toko ini ?",
+         icon: 'warning',
+         showCancelButton: true,
+         confirmButtonColor: '#3085d6',
+         cancelButtonColor: '#d33',
+         confirmButtonText: 'Yes, delete it!'
+         }).then((result) => {
+         if (result.isConfirmed) {
+               $.ajax({
+                  url: `{{route('delete-toko')}}`,
+                  headers: {
+                     'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                  },
+                  type: 'post',
+                  data:{'id_toko':id},
+                  success: function(response){
+                     $('.toko-yajra').DataTable().ajax.reload(null, true);
+                     Swal.fire(
+                           'Deleted!',
+                           'Your file has been deleted.',
+                           'success'
+                     )
+                  },
+                  error: function(err){
+                     console.log('error',err);
+                  }
+               })                
+         }
+   })
+}
 </script>
 @endpush
---
+++ resources/views/admin/toko/modal.blade.php
@@ -0,0 +1,18 @@
+
+
+
+
+<div class="modal2" id="modaltoko">
+    <div class="modal__wrapper">
+        <h3 id="title_modal">Input Toko</h3>
+        <p></p>
+        <form action="" id="form-toko">
+            <label id="id_label_nama_toko" class="text-label-modal" for="nama_barang" style="margin-left: 5px;">Nama Toko</label>
+            <input class="input-modal-global" id="nama_toko" required type="text" style="margin-top:5px;" name="nama_toko" placeholder="Nama toko">
+            <div class="btn-group-modal2">
+                <button type="submit" class="btn-modal2 btn-blue">Submit</button>
+                <button class="btn-modal2 btn-light-modal2 modal__close_2">Cancel</button>
+            </div>
+        </form>
+    </div>
+</div>
