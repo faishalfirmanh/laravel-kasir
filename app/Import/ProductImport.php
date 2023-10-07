915beb7b8b644bd2adc2689fa59c04b9880a39ed
isal48 <isalredarmy1@gmail.com>
2023-10-07 19:00:02 +0700

    fitur upload excel product

--- app/Import/ProductImport.php
+++ app/Import/ProductImport.php
@@ -3,7 +3,7 @@
 namespace App\Import;
 
 use App\Models\Product as ProdcutModels;
-
+use App\Models\ProductJual;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 
@@ -24,17 +24,35 @@ class ProductImport implements ToCollection, WithHeadingRow{
         $models = [];
         foreach ($rows as $row) 
         {
-            $nama_barang = $row['name_barang'];
-            $cabang_toko = $row['name_toko'];
-            $kategori = $row['kategori'];
-            $harga_beli = $row['harga_beli'];
-            $satuan_kg = $row['is_kg'];
-            $stock =  $row['total_stock'];
-            $expired =  $row['expired'];
+            $nama_barang = strip_tags(strtolower($row['name_barang']));
+            $cabang_toko = strip_tags(strtolower($row['name_toko']));
+            $kategori = strip_tags(strtolower($row['kategori']));
+            $harga_beli = strip_tags(strtolower($row['harga_beli']));
+            $satuan_kg = strip_tags(strtolower($row['is_kg']));
+            $stock =  strip_tags(strtolower($row['total_stock']));
+            $expired =  strip_tags(strtolower($row['expired']));
 
-            if ($satuan_kg !== 0 && $satuan_kg !== 1) {
+            $item_berat =  strip_tags(strtolower($row['satuan_berat_item']));
+            $harga_jual =  strip_tags(strtolower($row['harga_jual']));
+            $subnmae =  strip_tags(strtolower($row['subname']));
+
+            if (isset($item_berat) && $item_berat !== "") {
+                if (!is_numeric($item_berat)) {
+                    echo "error, satuan berat item ".$nama_barang." harus berupa angka";
+                    die();
+                }
+            }
+
+            if (isset($harga_jual) && $harga_jual !== "") {
+                if (!is_numeric($harga_jual)) {
+                    echo "error, harga jual ". $nama_barang ." harus berupa angka";
+                    die();
+                }
+            }
+
+            if ($satuan_kg !== "0" && $satuan_kg !== "1") {
                 $i = 400;
-                echo "error, input is kg harus 0 atau 1";
+                echo "error, input is kg harus 0 atau 1 "." product name ".$nama_barang;
                 die();
             }
             
@@ -43,30 +61,46 @@ class ProductImport implements ToCollection, WithHeadingRow{
             $cek_prd_name = cek_product_by_name($nama_barang);
 
             if ($cek_toko == null) {
-                echo "error, nama toko tidak ada";
+                echo "error, nama toko ".$cabang_toko." tidak ada";
                 die();
             }
 
             if ($cek_kategori == null) {
-                echo "error, nama kategpri tidak ada";
+                echo "error, nama kategpri ".$nama_barang ." tidak ada";
                 die();
             }
 
             if ($cek_prd_name != null) {
-                echo "error, nama prodicut sudah ada";
+                echo "error, nama prodicut ". $nama_barang." sudah ada";
                 die();
             }
             $cek_input_stock = $satuan_kg == 1 ? 'total_kg' : 'pcs';
             // try {
-                  ProdcutModels::create([
-                    'nama_product' => $nama_barang,
-                    'toko_id' =>  $cek_toko->id_toko,
-                    'kategori_id' => $cek_kategori->id_kategori,
-                    'harga_beli' =>  $harga_beli,
-                    'is_kg' => $satuan_kg,
-                     $cek_input_stock =>  $stock,
-                    'expired' =>  $expired
-                 ]);
+                $prod  =  ProdcutModels::create([
+                            'nama_product' => $nama_barang,
+                            'toko_id' =>  $cek_toko->id_toko,
+                            'kategori_id' => $cek_kategori->id_kategori,
+                            'harga_beli' =>  $harga_beli,
+                            'is_kg' => $satuan_kg,
+                            $cek_input_stock =>  $stock,
+                            'expired' =>  $expired
+                        ]);
+                
+                if ($harga_jual !== "" &&  $item_berat !==  "") {
+
+                    if ($harga_jual < $prod->fresh()->harga_beli ) {
+                        echo "error, harga beli ".$prod->fresh()->nama_product ." lebih dari harga jual";
+                        die();
+                    }
+
+                    ProductJual::create([
+                        'product_id' => $prod->fresh()->id_product,
+                        'subname' => $subnmae,
+                        'satuan_berat_item' => $item_berat, 
+                        'price_sell' => $harga_jual
+                    ]);
+                 }
+               
             // } catch (\Exception $e) {
             //     DB::rollBack();
             //     // $save_db = false;
--- resources/views/admin/uploadproduct/index.blade.php
+++ resources/views/admin/uploadproduct/index.blade.php
@@ -23,16 +23,64 @@
     <div style="margin-top: 20px">
         <a href="{{asset('downloadexample/example.xlsx')}}" style="font-size: 20px">download contoh file excel</a>
     </div>
-    <div style="margin-top: 20px">
-         <label for="file_upload"   style="margin-left: 5px;font-size: 14px">Pilih File excel</label>
-         <input type="file"  class="input-modal-global-dash" id="file_upload" style="margin-left: 5px;width:300px">
-    </div>
-    <div style="margin-top: 20px;margin-left:5px">
-         <input class="btn-modal2 btn-blue" id="id_submit" type="submit" value="Submit">
-    </div>
+    <form id="uploadProd" enctype="multipart/form-data">
+        <div style="margin-top: 20px">
+            <label for="file_upload"   style="margin-left: 5px;font-size: 14px">Pilih File excel</label>
+            <input type="file" name="file_excel"  class="input-modal-global-dash" id="file_upload" style="margin-left: 5px;width:300px">
+        </div>
+        <div style="margin-top: 20px;margin-left:5px">
+            <input class="btn-modal2 btn-blue" id="id_submit_upload" type="submit" value="Submit">
+        </div>
+    </form>
 </div>
 @endsection
 
 @section('modal_global')
     @include('admin.uploadproduct.modal')
 @endsection
+
+@push("scripts")
+    <script>
+        // $("#id_submit_upload").on("click", function(e) {
+
+        //     console.log("click upload");
+        // })
+
+        $(document).ready(function () {
+            $('#uploadProd').submit(function (e) {
+                e.preventDefault();
+                $.ajax({
+                    url: `{{ route('product-upload-excel') }}`,
+                    type: 'POST',
+                    beforeSend: function(xhr) {
+                        xhr.setRequestHeader('Authorization', `Bearer ${localStorage.getItem("token")}`);
+                    },
+                    data: new FormData(this),
+                    contentType: false,
+                    processData: false,
+                    success: function (response) {
+                        console.log(typeof(response));
+                        if (typeof(response) == 'string') {
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'Oops...',
+                                text: `${response}`,
+                            })
+                        }else{
+                            Swal.fire(
+                                'Create!',
+                                'Uplaod file excel product success.',
+                                'success'
+                            )
+                        }
+                        
+                    },
+                    error: function (error) {
+                        console.log("error ",error);
+                    }
+                });
+            });
+        });
+
+    </script>
+@endpush
